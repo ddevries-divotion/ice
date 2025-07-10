93b06a58ff4988c8c9b884fb65a46f237caa6529
Dennis de Vries <dennis.de.vries@divotion.com>
2025-07-10 10:33:16 +0200

    refactor: `IceAddTitlePlugin.js` to ESNext

--- src/plugins/IceAddTitlePlugin/IceAddTitlePlugin.js
+++ src/plugins/IceAddTitlePlugin/IceAddTitlePlugin.js
@@ -1,27 +1,55 @@
-(function () {
-  var exports = this,
-    IceAddTitlePlugin;
+// IceAddTitlePlugin.js - ES6 version
+// Copyright (c) The New York Times, CMS Group, Matthew DeLambo
+// Copyright (c) Divotion B.V., Conflux, Dennis de Vries
+// Licensed under the GNU General Public License v2.0 or later
 
-  IceAddTitlePlugin = function (ice_instance) {
+/**
+ * @class IceAddTitlePlugin
+ * @description Adds a title attribute to nodes indicating modification details.
+ */
+class IceAddTitlePlugin {
+  /**
+   * @param {Object} ice_instance - The ice instance.
+   */
+  constructor(ice_instance) {
     this._ice = ice_instance;
-  };
+  }
 
-  IceAddTitlePlugin.prototype = {
-    nodeCreated: function (node, option) {
-      node.setAttribute(
-        "title",
-        (option.action || "Modified") +
-          " by " +
-          node.getAttribute(this._ice.userNameAttribute) +
-          " - " +
-          ice.dom.date(
-            "m/d/Y h:ia",
-            parseInt(node.getAttribute(this._ice.timeAttribute)),
-          ),
-      );
-    },
-  };
+  /**
+   * Adds a title attribute to the node.
+   * @param {Element} node - The DOM node.
+   * @param {Object} option - Options containing action.
+   */
+  nodeCreated(node, option) {
+    node.setAttribute(
+      "title",
+      `${option.action || "Modified"} by ${node.getAttribute(this._ice.userNameAttribute)} - ${ice.dom.date(
+        "m/d/Y h:ia",
+        parseInt(node.getAttribute(this._ice.timeAttribute)),
+      )}`,
+    );
+  }
+}
 
-  ice.dom.noInclusionInherits(IceAddTitlePlugin, ice.IcePlugin);
-  exports._plugin.IceAddTitlePlugin = IceAddTitlePlugin;
-}).call(this.ice);
+/**
+ * Sets up inheritance for IceAddTitlePlugin from ice.IcePlugin.
+ * @see ice.IcePlugin
+ */
+ice.dom.noInclusionInherits(IceAddTitlePlugin, ice.IcePlugin);
+
+/**
+ * Exports the IceAddTitlePlugin for CommonJS and attaches it to the global ice object in browsers.
+ * @module IceAddTitlePlugin
+ */
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = IceAddTitlePlugin;
+}
+/**
+ * Attaches IceAddTitlePlugin to the global ice._plugin object in browsers.
+ * @global
+ */
+if (typeof window !== "undefined") {
+  window.ice = window.ice || {};
+  window.ice._plugin = window.ice._plugin || {};
+  window.ice._plugin.IceAddTitlePlugin = IceAddTitlePlugin;
+}
