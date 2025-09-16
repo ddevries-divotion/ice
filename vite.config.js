54cb17d336f3b4a92cd8a5155c8f86a1b6d74d15
Dennis de Vries <dennis.de.vries@divotion.com>
2025-09-16 09:59:41 +0200

    feat(demo): use Vite for serving the demo

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = resolve(fileURLToPath(import.meta.url), '..');
+
+const PUB_DIR = 'dist';
+
+export default defineConfig({
+  root: resolve(__dirname, 'demo'),
+  publicDir: resolve(__dirname, PUB_DIR),
+});
