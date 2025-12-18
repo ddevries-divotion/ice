ee313002b2492a78099c4f2030c5958ca2cc0e86
Dennis de Vries <dennis.de.vries@divotion.com>
2025-12-18 15:26:06 +0100

    chore: remove rangy dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,28 +2,36 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser },
+  },
   {
     files: ["**/*.{js,mjs,cjs}"],
     rules: {
       "no-undef": ["warn", { typeof: false }],
-      "no-unused-vars": ["error", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
+      "no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
     },
     languageOptions: {
       globals: {
         ice: "readonly",
         module: "writable", // Allow module for Node/CommonJS environments
         exports: "writable", // Allow exports for Node/CommonJS environments
-        rangy: "readonly", // Allow rangy for browser environments
         require: "readonly", // Optional: allow require if used
         jQuery: "readonly", // Allow jQuery for browser environments
         $: "readonly", // Allow $ shorthand for jQuery
         tinymce: "readonly", // Allow tinymce for TinyMCE plugin environments
-        range: "readonly" // Allow range for browser environments
-      }
-    }
-  }
+        range: "readonly", // Allow range for browser environments
+      },
+    },
+  },
 ]);
